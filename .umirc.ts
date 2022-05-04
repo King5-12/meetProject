18f95eae379097296e45835292e2ffdffc929e82
kim <jinweizhong@dragonest.com>
2022-05-04 22:31:19 +0800

    Feat: 删除模板渲染内容

--- .umirc.ts
+++ .umirc.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from 'umi';
 
-const WINDOW_CONFIG = {
-    API_HOST: process.env.API_HOST || 'http://172.20.90.123:10020',
-};
-
-const htmlExtraScript =
-    process.env.NODE_ENV === 'development'
-        ? `window.APP_CONFIG = ${JSON.stringify(WINDOW_CONFIG)}`
-        : `<%- htmlExtraScript %>`;
-
 export default defineConfig({
     dynamicImport: {},
     nodeModulesTransform: {
         type: 'none',
     },
-    headScripts: [htmlExtraScript],
     title: 'meetProject',
     favicon: '/favicon.ico',
     hash: true,
@@ -23,6 +13,6 @@ export default defineConfig({
         'border-radius-base': '4px',
     },
     fastRefresh: {},
-    
+
     ignoreMomentLocale: true,
 });
